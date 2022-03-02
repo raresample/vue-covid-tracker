09e7b2346592898e47c16e1be7a0948136efd88e
Aaron Hope <aghoecode@gmail.com>
2022-03-01 19:28:16 -0800

    Left off around t=39m because I can't seem to get the countries in the select options

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+    <DataBoxes :stats="stats" />
+    <CountrySelect :ountries="countries"/>
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-grey-500 text-3xl mt-10 mb-6">
+      Fetching Data
+      <img :src="loadingImage" class="w-24 m-auto" alt="">
+    </div>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
+  },
 }
 </script>
